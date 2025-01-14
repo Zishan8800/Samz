13e82895f24da1bfebbd1e770a204a7ad321d11a
Zishan8800 <122721834+Zishan8800@users.noreply.github.com>
2025-01-14 22:56:46 +0600

    Update rifat.js

--- scripts/cmds/rifat.js
+++ scripts/cmds/rifat.js
@@ -1,6 +1,6 @@
 module.exports = {
  config: {
-	 name: "rifat",
+	 name: "Sahel",
 	 version: "1.0",
 	 author: "AceGun",
 	 countDown: 5,
@@ -14,8 +14,8 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "rifat") {
  return message.reply({
- body: " 「❥︎----ღ᭄_ʜᴇʏ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.♡.\nᥫᩣ 𓆩Īⱦ'ꞩ Ꞧīӻⱥⱦ𓆪\n\n𝐁𝐎𝐓𝐎𝐖𝐍𝐄𝐑\n𝗥𝗔𝗙𝗜𝗬𝗔𝗡 𝗥𝗜𝗙𝗔𝗧」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/avlHVei.mp4")
+ body: " 「❥︎----ღ᭄_ʜᴇʏ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.♡.\nᥫᩣ 𓆩Īⱦ'ꞩ Ꞧīӻⱥⱦ𓆪\n\n𝐁𝐎𝐓𝐎𝐖𝐍𝐄𝐑\nꜱᴀʜᴇʟ ᴀʜᴍᴇᴅ」",
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/nocvHfz.mp4")
  });
  }
  }
